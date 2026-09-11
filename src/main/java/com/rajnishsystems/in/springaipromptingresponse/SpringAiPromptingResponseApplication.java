6e018b0c0ca0607443ce1cf67899b1f52fc7350a
Rajnish-chauhan <crajnish425@gmail.com>
2026-09-11 18:33:02 +0530

    Responses and Passing Prompt

---
+++ src/main/java/com/rajnishsystems/in/springaipromptingresponse/SpringAiPromptingResponseApplication.java
@@ -0,0 +1,13 @@
+package com.rajnishsystems.in.springaipromptingresponse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiPromptingResponseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiPromptingResponseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rajnishsystems/in/springaipromptingresponse/SpringAiPromptingResponseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rajnishsystems.in.springaipromptingresponse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiPromptingResponseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
